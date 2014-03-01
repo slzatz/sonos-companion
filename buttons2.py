d06a3f6a3e03520285d005606c1560bf195d9473
slzatz <slzatz@gmail.com>
2014-03-01 15:40:08 -0500

    Cleaned up buttons2.py - created main function and now detect keyboard interrupt

--- buttons2.py
+++ buttons2.py
@@ -2,7 +2,6 @@
 
 #note needs to be run as sudo python buttons.py to access GPIO 
 from time import sleep
-import sys
 import traceback
 import RPi.GPIO as GPIO
 from soco import SoCo
@@ -15,8 +14,8 @@ PANDORA = 18 #23
 
 GPIO.setmode(GPIO.BCM)
 
+# set up the button interface pins
 for n in [WSHU, WNYC, PANDORA]:
-
     GPIO.setup(n, GPIO.IN)
 
 sonos = SoCo('192.168.1.103')
@@ -47,8 +46,8 @@ GPIO.setup(SPICS, GPIO.OUT)
 potentiometer_adc = 0;
 
 last_read = 0       # this keeps track of the last potentiometer value
-tolerance = 5       # to keep from being jittery we'll only change
-                    # volume when the pot has moved more than 5 'counts'
+tolerance = 5       # to keep from being jittery we'll only change volume when the pot has moved more than 5 'counts'
+n = 0               # counter in while loop
 
 # read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
@@ -86,74 +85,53 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
     adcout >>= 1       # first bit is 'null' so drop it
     return adcout
 
-n=0        
+def main()     
  
-while True:
+    while True:
 
-    if GPIO.input(WNYC)==False:
-        station = 'wnyc'
-        play_uri('aac://204.93.192.135:80/wnycfm-tunein.aac', 'wnyc')
-            
-    if GPIO.input(PANDORA)==False:
-        station = 'quickmix'
-        play_uri('pndrradio:52877953807377986', 'quickmix') #, meta='')
-            
-    if GPIO.input(WSHU)==False:
-        station = 'wshu'
-        play_uri('x-rincon-mp3radio://wshu.streamguys.org/wshu-news', 'wshu')
-
-
-    # volume control code
-    # we'll assume that the pot didn't move
-    trim_pot_changed = False
-
-    # read the analog pin
-    trim_pot = readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
-    # how much has it changed since the last read?
-    pot_adjust = abs(trim_pot - last_read)
-
-    if ( pot_adjust > tolerance ):
-       trim_pot_changed = True
-
-    if 0:
-       print "trim_pot_changed", trim_pot_changed
-
-    if ( trim_pot_changed ):
-        set_volume = trim_pot / 10.24           # convert 10bit adc0 (0-1024) trim pot read into 0-100 volume level
-        set_volume = round(set_volume)          # round out decimal value
-        set_volume = int(set_volume)            # cast volume as integer
-
-        print 'Volume = {volume}%' .format(volume = set_volume)
-        set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' .format(volume = set_volume)
-        
-        sonos.volume = set_volume
+        # responding to push buttons
+        if GPIO.input(WNYC)==False:
+            station = 'wnyc'
+            play_uri('aac://204.93.192.135:80/wnycfm-tunein.aac', 'wnyc')
                 
+        if GPIO.input(PANDORA)==False:
+            station = 'quickmix'
+            play_uri('pndrradio:52877953807377986', 'quickmix') #, meta='')
+                
+        if GPIO.input(WSHU)==False:
+            station = 'wshu'
+            play_uri('x-rincon-mp3radio://wshu.streamguys.org/wshu-news', 'wshu')
 
-        if 0:
-            print "set_volume", set_volume
-            print "tri_pot_changed", set_volume
 
-        # save the potentiometer reading for the next loop
-        last_read = trim_pot
+        # volume control code
 
-    n+=1
-    if n == 1000:
-        if DEBUG:
-            print 'station set to '+station
-            print "trim_pot:", trim_pot
-            print "pot_adjust:", pot_adjust
-            print "last_read", last_read
+        # read the analog pin
+        trim_pot = readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
 
-        n = 0
+        if abs(trim_pot - last_read) > tolerance:
+ 
+            set_volume = trim_pot / 10.24           # convert 10bit adc0 (0-1024) trim pot read into 0-100 volume level
+            set_volume = round(set_volume)          # round out decimal value
+            set_volume = int(set_volume)            # cast volume as integer
 
-    sleep(0.1)
+            print 'Volume = {}%'.format(set_volume)
+            #sonos.volume = set_volume
+                    
+            last_read = trim_pot
 
-z = '''{'R.E.M. Radio': '637630342339192386', 'Nick Drake Radio': '409866109213435458', 'Dar Williams Radio': '1823409579416053314', 'My Morning Jacket Radio': '1776327778550113858', 'Patty
- Griffin Radio': '52876609482614338', 'Lucinda Williams Radio': '360878777387148866', 'Neil Young Radio': '52876154216080962', 'Wilco Radio': '1025897885568558658', 'The Decemberists
- Radio': '686295066286974530', 'The Innocence Mission Radio': '686869410788632130', 'Kris Delmhorst Radio': '610111769614181954', 'Counting Crows Radio': '1727297518525703746', 'Iron
- & Wine Radio': '686507220491527746', 'Bob Dylan Radio': '1499257703118366274', "slzatz's QuickMix": 'qm86206018', 'Ray LaMontagne Radio': '1726130468537198146', 'Vienna Teng Radio':
- '138764603804051010', 'QuickMix':'52877953807377986'}
-'''
+        n+=1
+        if n == 1000:
+            if DEBUG:
+                print 'station: {}'.format(station)
+                print 'volume: {}'.format(trim_pot)
+            n = 0
 
-meta = '&lt;DIDL-Lite xmlns:dc=&quot;http://purl.org/dc/elements/1.1/&quot; xmlns:upnp=&quot;urn:schemas-upnp-org:metadata-1-0/upnp/&quot;xmlns:r=&quot;urn:schemas-rinconnetworks-com:metadata-1-0/&quot; xmlns=&quot;urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/&quot;&gt;&lt;item id=&quot;OOOX{music_service_station_id}&quot; parentID=&quot;0&quot; restricted=&quot;true&quot;&gt;&lt;dc:title&gt;{music_service_station_title}&lt;/dc:title&gt;&lt;upnp:class&gt;object.item.audioItem.audioBroadcast&lt;/upnp:class&gt;&lt;desc id=&quot;cdudn&quot; nameSpace=&quot;urn:schemas-rinconnetworks-com:metadata-1-0/&quot;&gt;SA_RINCON3_{music_service_email}&lt;/desc&gt;&lt;/item&gt;&lt;/DIDL-Lite&gt;'.format(music_service = 'pndrradio', music_service_station_title = 'Patty Griffin Radio', music_service_station_id = '52876154216080962', music_service_email = 'slzatz@gmail.com')
+        sleep(0.1)
+
+
+try:
+    main()
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print "Keyboard Interrupt"
 
