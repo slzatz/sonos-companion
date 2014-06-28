6680e566c1b1b6b796ecbfce55892da90925ef0a
slzatz <slzatz@gmail.com>
2014-06-28 19:44:53 -0400

    More werkzeug debugging

--- sonos.py
+++ sonos.py
@@ -359,9 +359,9 @@ def spark():
     
     if track==prev_track:
         resp = make_response('',204) #204 = no content
-        #del resp.headers['content-type']
+        del resp.headers['content-type']
         resp.headers['Server'] = "sonos"
-        print "resp.headers=\n",resp.headers
+        #print "resp.headers=\n",resp.headers
         print resp
         return resp
     else:
