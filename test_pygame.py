c6471444bd0589fcde6aaf7a8b5e497d68294d8b
Steve rp2 <slzatz@gmail.com>
2014-08-05 23:20:52 +0000

    added

---
+++ test_pygame.py
@@ -0,0 +1,73 @@
+# see http://learn.adafruit.com/pi-video-output-using-pygmae/pointing-pygame-to-the-framebuffer
+
+import os
+import pygame
+import time
+import random
+
+# from https://github.com/adafruit/adafruit-pi-cam/blob/master/cam.py
+os.putenv('SDL_VIDEODRIVER', 'fbcon')
+os.putenv('SDL_FBDEV', '/dev/fb1')
+
+
+class pyscope :
+    screen = None;
+    
+    def __init__(self):
+        "Ininitializes a new pygame screen using the framebuffer"
+        # Based on "Python GUI in Linux frame buffer"
+        # http://www.karoltomala.com/blog/?p=679
+        disp_no = os.getenv("DISPLAY")
+        if disp_no:
+            print "I'm running under X display = {0}".format(disp_no)
+        
+        # Check which frame buffer drivers are available
+        # Start with fbcon since directfb hangs with composite output
+        drivers = ['fbcon', 'directfb', 'svgalib']
+        found = False
+        for driver in drivers:
+            # Make sure that SDL_VIDEODRIVER is set
+            if not os.getenv('SDL_VIDEODRIVER'):
+                os.putenv('SDL_VIDEODRIVER', driver)
+            try:
+                pygame.display.init()
+            except pygame.error:
+                print 'Driver: {0} failed.'.format(driver)
+                continue
+            found = True
+            break
+    
+        if not found:
+            raise Exception('No suitable video driver found!')
+        
+        size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+        print "Framebuffer size: %d x %d" % (size[0], size[1])
+        self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+        # Clear the screen to start
+        self.screen.fill((0, 0, 0))        
+        # Initialise font support
+        pygame.font.init()
+        # Render the screen
+        pygame.display.update()
+
+    def __del__(self):
+        "Destructor to make sure pygame shuts down, etc."
+
+    def test(self):
+        # Fill the screen with red (255, 0, 0)
+        red = (255, 0, 0)
+        self.screen.fill(red)
+        # Update the display
+        pygame.display.update()
+
+# Create an instance of the PyScope class
+scope = pyscope()
+scope.test()
+time.sleep(10)
+
+font = pygame.font.Font(None, 30)
+text_surface = font.render('pyScope (%s)' % "0.1", True, (255, 255, 255))
+scope.screen.blit(text_surface, (10, 0))
+pygame.display.update()
+time.sleep(20)
+
