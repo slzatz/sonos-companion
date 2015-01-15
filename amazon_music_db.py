f3240153aa1379b588fabaea0af882436988ff13
slzatz <slzatz@gmail.com>
2015-01-15 06:00:43 -0500

    add back amazon didl

--- amazon_music_db.py
+++ amazon_music_db.py
@@ -16,11 +16,11 @@ from sqlalchemy.schema import UniqueConstraint
 from sqlalchemy.exc import IntegrityError, OperationalError
 from sqlalchemy.orm.exc import NoResultFound
 
-#This is now defined in tft_sonos.py
+#This is now defined in tft_sonos.py but not in sonos_lcd_no_server3.py
 #only format item is {id_}
 didl_amazon = '''<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/"><item id="00030020{id_}" parentID="" restricted="true"><dc:title></dc:title><upnp:class>object.item.audioItem.musicTrack</upnp:class><desc id="cdudn" nameSpace="urn:schemas-rinconnetworks-com:metadata-1-0/">SA_RINCON6663_X_#Svc6663-0-Token</desc></item></DIDL-Lite>'''
 
-__all__ = ['Song', 'Artist', 'Image', 'session', 'IntegrityError',  'OperationalError', 'NoResultFound'] #,  'didl_amazon'] #, 'conn'] #, 'select', 'join', 'and_']
+__all__ = ['Song', 'Artist', 'Image', 'session', 'IntegrityError',  'OperationalError', 'NoResultFound', 'didl_amazon'] #, 'conn'] #, 'select', 'join', 'and_']
 
 Base = declarative_base()
 
