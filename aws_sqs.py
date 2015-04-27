9daeeb2be5cd888a50db0508ff338ff09047b66d
slzatz <slzatz@gmail.com>
2015-04-27 09:49:29 -0400

    Delete aws_sqs.py

--- aws_sqs.py
+++
@@ -1,7 +0,0 @@
-import boto.sqs
-from boto.sqs.message import Message
-conn = boto.sqs.connect_to_region(
-    "us-east-1",
-    aws_access_key_id='AKIAJZZMUDM7GMBJE2KQ',
-    aws_secret_access_key='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')
-
