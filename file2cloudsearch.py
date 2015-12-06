40a89ac4f0f0d60685a877985fdc321cd0e4510a
slzatz <slzatz@gmail.com>
2015-12-06 15:29:45 -0500

    Update to file2cloudearch but it's based on boto and more recent version now based on boto3

--- file2cloudsearch.py
+++ file2cloudsearch.py
@@ -8,12 +8,32 @@ There are more items then we need to upload to cloudsearch so we do process the 
 
     item = {k:item[k] for k in item if k in ('artist','album','title','uri', 'album_art')}
 
+Boto3 document format below
+[
+ {"type": "add",
+  "id":   "tt0484562",
+  "fields": {
+    "title": "The Seeker: The Dark Is Rising",
+    "directors": "Cunningham, David L.",
+    "genres": ["Adventure","Drama","Fantasy","Thriller"],
+    "actors": ["McShane, Ian","Eccleston, Christopher","Conroy, Frances",
+              "Crewson, Wendy","Ludwig, Alexander","Cosmo, James",
+              "Warner, Amelia","Hickey, John Benjamin","Piddock, Jim",
+              "Lockhart, Emma"]
+  }
+ },
+ {"type": "delete",
+  "id":   "tt0484575"
+ }
+]
 '''
 
 from boto.cloudsearch2.layer2 import Layer2
 from boto.cloudsearch2.domain import Domain
 import boto.cloudsearch2
-import boto3
+#import boto3
+#cloudsearchdomain = boto3.client('cloudsearchdomain', endpoint_url=c.aws_cs_url, region_name='us-east-1')
+#response = cloudsearchdomain.upload_documents( documents=b'bytes', contentType='application/json') # docs are json
 import sys
 import json
 import config as c
@@ -29,8 +49,10 @@ domain_data = (domain_data['DescribeDomainsResponse']
 
 domain = Domain(conn, domain_data[0])
 print(domain)
+print("\nNOTE: you need to open up the permissions by going to the Web dashboard of CloudSearch.\n")
+file_name = input("What file do you want to use for uploading data to CloudSearch ?")
 
-with open('rhapsody_tracks','r') as f:
+with open(file_name,'r') as f:
     z = f.read()
 
 full_items = json.loads(z)
@@ -39,8 +61,11 @@ for item in full_items:
     item = {k:item[k] for k in item if k in ('artist','album','title','uri', 'album_art')}
     items.append(item)
 
+#documents = json.dumps(items) #boto3
+
 n = 0
 while True:
+    # there are limitations in how many docs can be uploaded in a batch but it's more than 100
     cur_items = items[n:n+100]
 
     if not cur_items:
@@ -56,8 +81,8 @@ while True:
 
     try:    
         resp=doc_service.commit()
-        print(resp)
+        print(resp) #<boto.cloudsearch2.document.CommitResponse object at 0x027EF1D0>
     except Exception as e:
-        print(e)
+        print("Exception trying to write CloudSearch: ", e) #The cryptic exception will be 'adds' if permissions issue
 
     n+=100
