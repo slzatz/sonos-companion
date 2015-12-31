217cd262e21dc110295671f9f4728250c5238de7
slzatz <slzatz@gmail.com>
2015-12-30 20:48:34 -0500

    Changes to echo_check_solr.py to deal with updates to Solr index and first commit of echo_check_pysolr.py that uses pysolr for python2.7

--- echo_check_pysolr.py
+++ echo_check_pysolr.py
@@ -1,12 +1,10 @@
 '''
-uses SolrClient library, which was billed as ?only working with python3
-so this can be used on most systems but since raspberry pi support for python3
-is not great, I also have a version called echo_check_pysolr.py that (surprise)
-uses pysolr, which works fine on python2 but has a different syntax than SolrClient
+created to have a python2.7 version of echo check that uses solr
+uses pysolr while the python3 version:  echo_check_solr.py uses SolrClient
 
 uses boto3
 uses dynamodb for radio stations and shuffling songs
-uses cloudsearch to enable searching on tracks and artists
+uses solr and pysolr, which works on python 2.7 to enable searching on tracks and artists
 
 PlayStation play {myartist} radio
 PlayStation play {myartist} pandora
@@ -32,6 +30,13 @@ current_track = master.get_current_track_info() --> {
             u'duration': '0:02:45', 
             u'position': '0:02:38', 
             u'album_art': 'http://cont-ch1-2.pandora.com/images/public/amz/3/2/9/3/655037093923_500W_500H.jpg'}
+
+    >>> z = solr.search("artist:Cat AND artist:Stevens")
+    >>> z
+    <pysolr.Results object at 0x02C792F0>
+    >>> for r in z:
+    ...     print r['title'],r['album'],r['artist'],r['uri']
+
 To Do:
 - Artist shuffling could move from dynamodb to cloudsearch: result = cloudsearchdomain.search(query=task['artist'], queryOptions='{"fields":["artist"]}')
 - Could also have an Album search -> Album play album {after the gold rush|albumtitle} - could be custom slot but not sure any real benefit
@@ -52,10 +57,7 @@ import soco
 from soco import config
 import boto3 
 import config as c
-from SolrClient import SolrClient
-
-solr = SolrClient(c.ec_uri+':8983/solr')
-collection = 'sonos_companion'
+import pysolr
 
 parser = argparse.ArgumentParser(description='Command line options ...')
 parser.add_argument('--player', '-p', default='all', help="This is the name of the player you want to control or all")
@@ -67,18 +69,18 @@ location = object.get()['Body'].read()
 print("The current location is {}".format(location))
 
 sqs = boto3.resource('sqs', region_name='us-east-1') 
-queue_name = 'echo_sonos_ct' if location==b'ct' else 'echo_sonos'
+queue_name = 'echo_sonos_ct' if location=='ct' else 'echo_sonos'
 sqs_queue = sqs.get_queue_by_name(QueueName=queue_name) 
 
-#cloudsearchdomain = boto3.client('cloudsearchdomain', endpoint_url=c.aws_cs_url, region_name='us-east-1')
-
+solr = pysolr.Solr(c.ec_uri+':8983/solr/sonos_companion/', timeout=10)
+    
 config.CACHE_ENABLED = False
 
-print("\n")
+print "\n"
 n = 0
 while 1:
     n+=1
-    print("attempt "+str(n))
+    print "attempt "+str(n)
     sp = soco.discover(timeout=5)
     if sp:
         break
@@ -87,15 +89,15 @@ while 1:
 
 if args.player=='all':
     d = {}
-    print("\nSpeakers->")
+    print "\nSpeakers->"
     for s in sp:
-        print(s.player_name)
+        print s.player_name
         gc = s.group.coordinator
         d[gc] = d[gc] + 1 if gc in d else 1
 
-    print("\nGroup Coordinators->")
+    print "\nGroup Coordinators->"
     for gc in d:
-        print("{}:{}".format(gc.player_name, d[gc]))
+        print "{}:{}".format(gc.player_name, d[gc])
 
     master = max(d.keys(), key=lambda k: d[k])
 
@@ -105,12 +107,12 @@ else:
             master = s
             break
     else:
-        print("{} is not a speaker".format(args.player))
+        print "{} is not a speaker".format(args.player)
         sys.exit(1)
 
-print("\nmaster = ", master.player_name)
+print "\nmaster = ", master.player_name
 
-print("\nprogram running ...")
+print "\nprogram running ..."
 
 meta_format_pandora = '''<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/"><item id="OOOX52876609482614338" parentID="0" restricted="true"><dc:title>{title}</dc:title><upnp:class>object.item.audioItcast</upnp:class><desc id="cdudn" nameSpace="urn:schemas-rinconnetworks-com:metadata-1-0/">{service}</desc></item></DIDL-Lite>'''
 
@@ -140,14 +142,14 @@ def play_deborah_radio(num):
         songs.append(DEBORAH_TRACKS[n])
 
     for uri in songs:
-        print(uri)
+        print uri
         i = uri.find('amz')
         ii = uri.find('.')
         id_ = uri[i:ii]
-        print(id_)
+        print id_
         meta = didl_amazon.format(id_=id_)
         my_add_to_queue('', meta)
-        print("---------------------------------------------------------------")
+        print "---------------------------------------------------------------"
 
     master.play_from_queue(0)
 
@@ -172,24 +174,24 @@ COMMON_ACTIONS = {'pause':'pause', 'resume':'play', 'skip':'next'}
 
 while 1:
     
-    print("{} - checking sqs queue: {}; sonos master speaker: {}".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), queue_name, master.player_name))
+    print "{} checking sqs queue; {}".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), master.player_name)
 
     # check sqs_queue for new actions to take
     try:
         r = sqs_queue.receive_messages(MaxNumberOfMessages=1, VisibilityTimeout=0, WaitTimeSeconds=20) 
     except Exception as e:
-        print("sqs receive error checking for posted actions: ", e)
+        print "sqs receive error checking for posted actions: ", e
         continue
 
     if r:
         m = r[0]
         body = m.body
-        print("sqs messge body =", body)
+        print "sqs messge body =", body
 
         try:
             task = json.loads(body)
         except Exception as e:
-            print("error reading the sqs message body: ", e)
+            print "error reading the sqs message body: ", e
             m.delete()
             continue
 
@@ -211,7 +213,7 @@ while 1:
                 station = STATIONS.get(task['station'].lower())
                 if station:
                     uri = station[1]
-                    print("uri=",uri)
+                    print "uri=",uri
                     if uri.startswith('pndrradio'):
                         meta = meta_format_pandora.format(title=station[0], service=station[2])
                         master.play_uri(uri, meta, station[0]) # station[0] is the title of the station
@@ -220,26 +222,25 @@ while 1:
                         meta = meta_format_radio.format(title=station[0], service=station[2])
                         master.play_uri(uri, meta, station[0]) # station[0] is the title of the station
                 else:
-                    print("{} radio is not a preset station.".format(task['station']))
+                    print "{} radio is not a preset station.".format(task['station'])
 
         elif action in ('play','add') and task.get('trackinfo'): 
 
             #The query below only searches title and artist fields so you don't get every song on After the Gold Rush
-            #result = cloudsearchdomain.search(query=task['trackinfo'], queryOptions='{"fields":["title", "artist"]}')
             s = 'artist:' + ' artist:'.join(task['trackinfo'].split()) + ' title:' + ' title:'.join(task['trackinfo'].split())
-            result = solr.query(collection, {'q':s, 'rows':1}) #..'rows':25 ...}, queryOptions='{"fields":["title", "artist"]}')
+            result = solr.search(s, **{'rows':1})
 
-            if result.get_results_count():
-                track = result.data['response']['docs'][0]
+            if len(result):
+                track = result.docs[0]
                 try:
-                    print('artist: ' + track.get('artist', ['No artist']))
-                    print('album: ' + track.get('album', ['No album']))
-                    print('song: ' + track.get('title', ['No title']))
+                    print 'artist: ' + track.get('artist', 'No artist')
+                    print 'album: ' + track.get('album', 'No album')
+                    print 'song: ' + track.get('title', 'No title')
                 except Exception as e:
-                    print("Unicode error")
-                uri = track.get('uri', [''])
-                print('uri: ' + uri)
-                print("---------------------------------------------------------------")
+                    print "Unicode error"
+                uri = track.get('uri', '')
+                print 'uri: ' + uri
+                print "---------------------------------------------------------------"
 
                 if 'amz' in uri:
                     i = uri.find('amz')
@@ -251,8 +252,8 @@ while 1:
                     ii = uri.find('.',i)
                     id_ = uri[i:ii]
                     meta = didl_rhapsody.format(id_=id_)
-                    print('---------------------------------------------------------------')
-                    print('meta: ',meta)
+                    print '---------------------------------------------------------------'
+                    print 'meta: ',meta
 
                 if action == 'play':
                     master.stop()
@@ -264,26 +265,26 @@ while 1:
                     my_add_to_queue('', meta)
 
             else:
-                print("Could not find requested track " + task['trackinfo'])
+                print "Could not find requested track " + task['trackinfo']
 
         elif action == 'play_album' and task.get('album'): 
 
             s = 'album:' + ' AND album:'.join(task['album'].split())
-            result = solr.query(collection, {'q':s, 'rows':25})
+            result = solr.search(s, **{'rows':25}) #only brings back actual matches but 25 seems like max for most albums
 
-            if result.get_results_count():
+            if len(result):
                 master.stop()
                 master.clear_queue()
-                for track in result.data['response']['docs']:
+                for track in result:
                     try:
-                        print('artist: ' + track.get('artist', ['No artist']))
-                        print('album: ' + track.get('album', ['No album']))
-                        print('song: ' + track.get('title', ['No title']))
+                        print 'artist: ' + track.get('artist', 'No artist')
+                        print 'album: ' + track.get('album', 'No album')
+                        print 'song: ' + track.get('title', 'No title')
                     except Exception as e:
-                        print("Unicode error")
-                    uri = track.get('uri', [''])
-                    print('uri: ' + uri)
-                    print("---------------------------------------------------------------")
+                        print "Unicode error"
+                    uri = track.get('uri', '')
+                    print 'uri: ' + uri
+                    print "---------------------------------------------------------------"
 
                     if 'amz' in uri:
                         i = uri.find('amz')
@@ -295,34 +296,34 @@ while 1:
                         ii = uri.find('.',i)
                         id_ = uri[i:ii]
                         meta = didl_rhapsody.format(id_=id_)
-                        print('---------------------------------------------------------------')
-                        print('meta: ',meta)
+                        print '---------------------------------------------------------------'
+                        print 'meta: ',meta
 
                     my_add_to_queue('', meta)
 
                 master.play_from_queue(0)
 
             else:
-                print("Could not find requested album " + task['album'])
+                print "Could not find requested album " + task['album']
 
         elif action == 'shuffle' and task.get('artist') and task.get('number'):
 
             try:
                 number = int(task['number'])
             except ValueError as e:
-                print(e)
+                print e
                 number = 8
 
-            #result = cloudsearchdomain.search(query=task['artist'], queryOptions='{"fields":["artist"]}', size=500)
             s = 'artist:' + ' AND artist:'.join(task['artist'].split())
-            result = solr.query(collection, {'q':s, 'rows':500}) #..'rows':25 ...}, queryOptions='{"fields":["title", "artist"]}')
+            result = solr.search(s, **{'rows':500}) 
 
-            count = result.get_results_count()
+            count = len(result)
+            print "Total track count for {} was {}".format(task['artist'], count)
+
             if count:
                 master.stop()
                 master.clear_queue()
-                tracks = result.data['response']['docs']
-                print("track count =",count)
+                tracks = result.docs
                 k = number if number <= count else count
                 picks = []
                 for j in range(k):
@@ -333,14 +334,14 @@ while 1:
                             break
                     track = tracks[n]
                     try:
-                        print('artist: ' + track.get('artist', 'No artist'))
-                        print('album: ' + track.get('album', 'No album'))
-                        print('song: ' + track.get('title', 'No title'))
+                        print 'artist: ' + track.get('artist', 'No artist')
+                        print 'album: ' + track.get('album', 'No album')
+                        print 'song: ' + track.get('title', 'No title')
                     except Exception as e:
-                        print("Unicode error")
+                        print "Unicode error"
                     uri = track.get('uri', '')
-                    print('uri: ' + uri)
-                    print('---------------------------------------------------------------')
+                    print 'uri: ' + uri
+                    print '---------------------------------------------------------------'
 
                     if 'amz' in uri:
                         i = uri.find('amz')
@@ -364,7 +365,7 @@ while 1:
             try:
                 getattr(master, COMMON_ACTIONS[action])()
             except soco.exceptions.SoCoUPnPException as e:
-                print("master.{}:".format(action), e)
+                print "master.{}:".format(action), e
 
         elif action in ('quieter','louder'):
             volume = master.volume
@@ -372,16 +373,16 @@ while 1:
             new_volume = volume - 10 if action=='quieter' else volume + 10
             
             if args.player == 'all':
-                for s in sp:
+                for s in speakers:
                     s.volume = new_volume
             else:
                 master.volume = new_volume
 
-            print("args.player=",args.player)
-            print("I tried to make the volume "+action)
+            print "args.player=",args.player
+            print "I tried to make the volume "+action
 
         else:
-            print("I have no idea what you said")
+            print "I have no idea what you said"
 
     sleep(0.5)
 
