fe8b5199815d4a70f50d25f307fec53ffab6c9c8
slzatz <slzatz@gmail.com>
2016-04-04 16:55:32 -0400

    esp8266/main.py: receive scrobbles

--- esp8266/main.py
+++ esp8266/main.py
@@ -7,6 +7,7 @@ p2 = Pin(2, Pin.IN)
 
 def run():
   p.wlan_connect()
+  n = 0
   while 1:
     if not p0.value():
       p.publish('sonos/ct', '{"action":"louder"}')
@@ -14,5 +15,14 @@ def run():
       p.publish('sonos/ct', '{"action":"quieter"}')
 
     sleep_ms(100)
+
+    n+=1
+    if n = 10:
+      socket.send('GET /sonos_track HTTP/1.0\r\n\r\n')
+      z = s.recv(4096)
+      print(z)
+      n = 0
+    else:
+      sleep_ms()
 
 run()
