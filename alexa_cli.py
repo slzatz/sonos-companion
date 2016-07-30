6fe0556292e3da4b17075cd664ed73be7a1cb816
slzatz <slzatz@gmail.com>
2016-07-30 11:18:31 -0400

    alexa_cli.py: cleanup and added volume commands

--- alexa_cli.py
+++ alexa_cli.py
@@ -1,3 +1,13 @@
+'''
+Below is the relationship between words entered at the command line and Amazon Intents (similar to Alexa mapping)
+louder,quieter->TurnTheVolume
+album->PlayAlbum, 
+pause->AMAZON.PauseIntent, 
+resume->AMAZON.ResumeIntent, 
+shuffle->Shuffle,
+radio->PlayStation
+everything else assumed to be the title of a track -> PlayTrack
+'''
 import requests
 import sys
 from config import ngrok_urls
@@ -7,8 +17,7 @@ url = ngrok_urls.get(location)
 if not url:
     sys.exit()
 
-intents = {'shuffle':'Shuffle', 'track':"PlayTrack", 'album':'PlayAlbum', 'radio':'PlayStation', 'pause':'AMAZON.PauseIntent', 'resume':'AMAZON.ResumeIntent'}
-slots = {'shuffle':'myartist', 'radio':'mystation', 'album':'myalbum', 'track':'mytitle'}
+slots = {'Shuffle':['myartist'], 'PlayStation':['mystation'], 'PlayAlbum':['myalbum'], 'PlayTrack':['mytitle', 'myartist'], 'TurnTheVolume':['volume'], 'AMAZON.ResumeIntent':[], 'AMAZON.PauseIntent':[]}
 
 while 1:
     try:
@@ -18,48 +27,49 @@ while 1:
         if 'play' in words:
             words.remove('play')
 
-        if 'pause' in words:
-            intent = 'pause'
-            value = ''
+        if 'louder' in words:
+            intent = 'TurnTheVolume'
+            values = ['up']
+        elif 'quieter' in words:
+            intent = 'TurnTheVolume'
+            values = ['down']
+        elif 'pause' in words:
+            intent = 'AMAZON.PauseIntent'
         elif 'resume' in words:
-            intent = 'resume'
-            value = ''
+            intent = 'AMAZON.ResumeIntent'
         elif 'shuffle' in words:
+            intent = 'Shuffle'
             words.remove('shuffle')
-            value = " ".join(words)
-            intent = 'shuffle'
+            values = [" ".join(words)]
         elif 'radio' in words:
+            intent = 'PlayStation'
             words.remove('radio')
-            value = " ".join(words)
-            intent = 'radio'
+            values = [" ".join(words)]
         elif 'album' in words:
+            intent = 'PlayAlbum'
             words.remove('album')
-            value = " ".join(words)
-            intent = 'album'
+            values = [" ".join(words)]
         else:
             if 'by' in words:
                 indx = words.index('by')
-                value = " ".join(words[:indx])
+                value0 = " ".join(words[:indx])
                 value1 = " ".join(words[indx+1:])
             else:
-                value = " ".join(words)
+                value0 = " ".join(words)
                 value1 = ''
 
-            intent = 'track'
+            values = [value0,value1]
+            intent = 'PlayTrack'
 
-        print("intent = {}".format(intent).encode('cp1252', errors='ignore'))
-        print("value = {}".format(value).encode('cp1252', errors='ignore'))
-        
-        if value:
-            slot_name = slots[intent]
-            slot_dict = {slot_name:{'name':slot_name, 'value':value}}
-            if intent == 'track':
-                slot_dict.update({'myartist':{'name':'myartist', 'value':value1}})
-        else:
-            slot_dict = {}
+        #print("intent = {}".format(intent).encode('cp1252', errors='ignore'))
 
-        intent_name = intents[intent]
-        data = {'session':{}, 'request':{'type':'IntentRequest', 'intent':{'slots':slot_dict, 'name':intent_name}}}
+        slot_dict = {}
+        for i,slot in enumerate(slots[intent]):
+            s = {slot:{'name':slot, 'value':values[i]}}
+            slot_dict.update(s)
+            #print("value = {}".format(values[i]).encode('cp1252', errors='ignore'))
+
+        data = {'session':{}, 'request':{'type':'IntentRequest', 'intent':{'slots':slot_dict, 'name':intent}}}
         print("data= {}".format(data).encode('cp1252', errors='ignore'))
         r = requests.post(url, json=data)
         print(r.text)
