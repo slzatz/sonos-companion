b31dcc7929d5316cbddcd8ae63a82292143aaaa9
slzatz <slzatz@gmail.com>
2016-09-05 17:32:34 -0400

    queue2solr.py: docstring changes

--- queue2solr.py
+++ queue2solr.py
@@ -1,10 +1,13 @@
 '''
 02-27-2016
 THIS IS CURRENTLY THE SCRIPT TO USE TO MOVE TRACKS IN THE QUEUE INTO SOLR
+The solr database is determined by the solr_uri in config.py - right now ec2
 This works without having to play the tracks
-RIGHT NOW THE PROGRAM EXPECTS THAT THESE ARE ALBUMS THAT REQUIRE TRACK NUMBERS
+The script asks whether this is an album to know whether to assign track numbers
+from the queue positions or just default to track 1
 Note that if the solr document already exists, the add operation will replace it. 
-
+However, if you change the id at all by changing the album name or title it won't delete
+the older versions.
 The upload format for SolrClient (python3 solr client program) is jsonifying a list of dictionaries:
 [{'id':'After_The_Gold_Rush_Birds', 'artist':'Neil Young', 'title':'Birds', 'album':'After the Gold Rush',
 'uri':'x-sonos-http:amz%3atr%3a44ce93d2-4105-416a-a905-51fe0f38ed9a.mp4?sid=26&flags=8224&sn=2'...}{...
