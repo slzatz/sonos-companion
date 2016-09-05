1c282bb61978b6aee9e178193f8e087decc216cf
slzatz <slzatz@gmail.com>
2016-09-05 17:30:59 -0400

    amazon_music_db.py:  Some changes related to moving database to ec2

--- amazon_music_db.py
+++ amazon_music_db.py
@@ -30,22 +30,22 @@ from sqlalchemy.orm.exc import NoResultFound
 #only format item is {id_}
 didl_amazon = '''<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/"><item id="00030020{id_}" parentID="" restricted="true"><dc:title></dc:title><upnp:class>object.item.audioItem.musicTrack</upnp:class><desc id="cdudn" nameSpace="urn:schemas-rinconnetworks-com:metadata-1-0/">SA_RINCON6663_X_#Svc6663-0-Token</desc></item></DIDL-Lite>'''
 
-__all__ = ['Song', 'Artist', 'Image', 'session', 'IntegrityError',  'OperationalError', 'NoResultFound', 'didl_amazon', 'engine'] #, 'conn'] #, 'select', 'join', 'and_']
+__all__ = ['Base','Artist', 'Image', 'session', 'IntegrityError',  'OperationalError', 'NoResultFound', 'didl_amazon', 'engine', 'create_engine', 'sessionmaker'] #, 'conn'] #, 'select', 'join', 'and_', 'Song']
 
 Base = declarative_base()
 
-class Song(Base):
-    __tablename__ = 'songs'
-    id = Column(Integer, primary_key=True)
-    artist = Column(String)
-    album = Column(String)
-    title = Column(String)
-    uri = Column(String)
-    album_art = Column(String)
-    __table_args__ = (UniqueConstraint('artist', 'album', 'title'),)
-    
-    def __repr__(self):
-        return "<artist={}, album={}, title={}, uri={}>".format(self.artist, self.album, self.title, self.uri)
+#class Song(Base):
+#    __tablename__ = 'songs'
+#    id = Column(Integer, primary_key=True)
+#    artist = Column(String)
+#    album = Column(String)
+#    title = Column(String)
+#    uri = Column(String)
+#    album_art = Column(String)
+#    __table_args__ = (UniqueConstraint('artist', 'album', 'title'),)
+#    
+#    def __repr__(self):
+#        return "<artist={}, album={}, title={}, uri={}>".format(self.artist, self.album, self.title, self.uri)
 
 class Artist(Base):
     __tablename__ = 'artists'
