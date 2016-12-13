dcb6fe16217c0e3ba404013cf4834a1ba6cc4b92
slzatz <slzatz@gmail.com>
2016-12-13 12:58:26 -0500

    artist_images_pyg.py - update for use with amazon_music_db.py

--- artist_images_pyg.py
+++ artist_images_pyg.py
@@ -15,7 +15,8 @@ import os
 import random
 import wand.image
 import textwrap
-from artist_images_db import *
+#from artist_images_db import *
+from amazon_music_db import *
 import lxml.html
 
 wrapper = textwrap.TextWrapper(width=72, replace_whitespace=False)  
@@ -66,6 +67,53 @@ def get_artist_info(artist, autocorrect=0):
         print("Exception in get_artist_info: ", e)
         return ''
 
+def get_artist_images(name):
+
+    print("**************Google Custom Search Engine Request for "+name+"**************")
+    http = httplib2.Http()
+    service = discovery.build('customsearch', 'v1',  developerKey=google_api_key, http=http)
+    z = service.cse().list(q=name, searchType='image', imgType='face', imgSize='xlarge', num=10, cx='007924195092800608279:0o2y8a3v-kw').execute() 
+
+    try:
+        a = session.query(Artist).filter(func.lower(Artist.name)==name.lower()).one()
+    except NoResultFound:
+        print("Don't have that name in db so created it")
+        a = Artist()
+        a.name = name
+        session.add(a)
+        session.commit()
+    except Exception as e:
+        print("a = session.query(Artist).filter(func.lower.. error:", e) 
+        return []
+
+    # must delete images before you can add new whole new set of images
+    session.query(Image).filter_by(artist_id=a.id).delete()
+    session.commit()
+
+    # the below works and deletes the images one at a time
+    #for image in a.images:
+    #    # with change in cascade don on 12-11-2016 may be possible to do a.images = [];session.commit()
+    #    print image.link, image.width, image.height,image.ok
+    #    session.delete(image)
+    #session.commit()    
+
+    images = []
+
+    for data in z['items']:
+        
+        image=Image()
+        image.link = data['link']
+        image.width = data['image']['width']
+        image.height = data['image']['height']
+        image.ok = True
+        images.append(image)
+
+    a.images = images
+    session.commit()
+            
+    print("images = ", images)
+    return images 
+
 #following not in use    
 def get_artist_image(artist,image):
 
@@ -75,7 +123,7 @@ def get_artist_image(artist,image):
         response = requests.get(url)
     except Exception as e:
         print( "response = requests.get(url) generated exception:", e)
-        image.status = False
+        image.ok = False
         print("changed image status to False")
         session.commit()
         img = wand.image.Image(filename = "test.bmp")
@@ -86,44 +134,67 @@ def get_artist_image(artist,image):
         except Exception as e:
             img = wand.image.Image(filename = "test.bmp")
             print ("img = wand.image.Image(file=StringIO(response.content)) generated exception:", e)
-            image.status = False
+            image.ok = False
             session.commit()
 
     size = str(DISPLAY[0])+'x'+str(DISPLAY[1])+'^'
     img.transform(resize = size)
     img = img.convert('bmp')
-    img.save(filename = "test1.bmp")
+    #img.save(filename = "test1.bmp")
     img = pygame.image.load("test1.bmp").convert()
 
     return img
 
-def display_artist_image(artist, image):
+def display_artist_image(artist_name, image):
 
-    #image = session.query(Image).join(Artist).filter(Artist.name == artist)[i] #.all()
+    #image = session.query(Image).join(Artist).filter(Artist.name == artist_name)[i] #.all()
     url = image.link
     try:
         response = requests.get(url)
-    except Exception as detail:
-        print( "response = requests.get(url) generated exception:", detail)
-        image.status = False
-        print("changed image status to False")
+    except Exception as e:
+        print( "response = requests.get(url) generated exception:", e)
+        image.ok = False
+        print("changed image ok to False")
         session.commit()
-        img = wand.image.Image(filename = "test.bmp")
+        return
+        #img = wand.image.Image(filename = "test.bmp")
     else:
 
         try:
             img = wand.image.Image(file=StringIO(response.content))
-        except Exception as detail:
-            img = wand.image.Image(filename = "test.bmp")
-            print ("img = wand.image.Image(file=StringIO(response.content)) generated exception:", detail)
-            image.status = False
+        except Exception as e:
+            print ("img = wand.image.Image(file=StringIO(response.content)) generated exception:", e)
+            print("artist=",artist_name)
+            print("image=",image)
+            image.ok = False
             session.commit()
+            return
 
     size = str(DISPLAY[0])+'x'+str(DISPLAY[1])+'^'
-    img.transform(resize = size)
-    img = img.convert('bmp')
-    img.save(filename = "test1.bmp")
-    img = pygame.image.load("test1.bmp").convert()
+
+    try:
+        img.transform(resize = size)
+        img = img.convert('bmp')
+    except Exception as e:
+        print("img.transform or img.convert error:", e)
+        print("artist=",artist_name)
+        print("image=",image)
+        image.ok = False
+        session.commit()
+        return
+
+    f = StringIO()
+    try:
+        #img.save(filename = "test1.bmp")
+        img.save(f)
+    except wand.exceptions.OptionError as e:
+        print("Problem saving image:", e)
+        return
+
+    f.seek(0)
+    #img = pygame.image.load("test1.bmp").convert()
+    img = pygame.image.load(f, 'bmp').convert()
+    f.close()
     
     screen.fill((0,0,0)) 
     screen.blit(img, (0,0))      
@@ -136,7 +207,7 @@ def display_artist_image(artist, image):
     font = pygame.font.SysFont('Sans', 20)
     font.set_bold(True)
     
-    text1 = font.render("Artist: "+artist, True, (255, 0, 0))
+    text1 = font.render("Artist: "+artist_name, True, (255, 0, 0))
     
     screen.fill((0,0,0)) 
     screen.blit(img, (0,0))      
@@ -144,7 +215,7 @@ def display_artist_image(artist, image):
 
     font = pygame.font.SysFont('Sans', 16)
  
-    text2 = get_artist_info(artist)
+    text2 = get_artist_info(artist_name)
     text2 = wrapper.fill(text2)
     lines = text2.split('\n')
     
@@ -156,8 +227,9 @@ def display_artist_image(artist, image):
 
     pygame.display.flip()
 
-    os.remove("test1.bmp") 
+    #os.remove("test1.bmp") 
 
+# not in use
 def show_artist():
     img.set_alpha(75) #0 - 100 the lower the number the more faded 
 
@@ -172,24 +244,33 @@ def show_artist():
 
     pygame.display.flip()
 
-    os.remove("test1.bmp") 
+    #os.remove("test1.bmp") 
 
 if __name__ == '__main__':
 
     SHOWNEWIMAGE = USEREVENT+1
-    artists = session.query(Artist)
-    L = artists.count()
+    artists = session.query(Artist).all()
+    L = len(artists)
     print("Number of artists = {}".format(L))
     
-    pygame.time.set_timer(SHOWNEWIMAGE, 20000)
+    pygame.time.set_timer(SHOWNEWIMAGE, 2000)
 
     while 1:
 
-        if pygame.event.get(SHOWNEWIMAGE):
-            artist = artists[random.randrange(0,L-1)].name
-            N = session.query(Image).join(Artist).filter(and_(Artist.name == artist, Image.status == True)).count()
-            image = session.query(Image).join(Artist).filter(and_(Artist.name == artist, Image.status == True))[random.randrange(0,N-1)] 
-            display_artist_image(artist, image) 
+        #if pygame.event.get(SHOWNEWIMAGE):
+            #artist_name = artists[random.randrange(0,L-1)].name
+            #N = session.query(Image).join(Artist).filter(and_(Artist.name==artist_name, Image.ok == True)).count()
+            #if N:
+            #    image = session.query(Image).join(Artist).filter(and_(Artist.name == artist, Image.ok == True))[random.randrange(0,N-1)] 
+            #    display_artist_image(artist_name, image) 
 
-        sleep(.1)
+        if pygame.event.get(SHOWNEWIMAGE):
+            #artist = artists[random.randrange(0,L-1)]
+            artist = random.choice(artists)
+            images = artist.images
+            if images:
+                #image = images[random.randrange(0,len(images)-1)]
+                image = random.choice(images)
+                display_artist_image(artist.name, image) 
+        sleep(1)
 
