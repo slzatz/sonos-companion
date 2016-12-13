62c9d6ad5e4db3023cd333f294c3ac8a7d7c5915
slzatz <slzatz@gmail.com>
2016-12-13 17:48:46 -0500

    artist_images_db.py: the is the renamed amazon_music_db.py, which was very similar to the original artist_images_db.py

--- artist_images_db.py
+++ artist_images_db.py
@@ -1,72 +1,82 @@
+'''
+This script is imported to enable access now to the AWS EC2 artist_images db
+The lines below may be better way to pull images (uses xlarge and face)
+service = discovery.build('customsearch', 'v1',  developerKey=g_api_key, http=http)
+z = service.cse().list(q=artist, start=1, imgType='face', searchType='image', imgSize='xlarge', num=10, cx='007924195092800608279:0o2y8a3v-kw').execute() 
+'''
 #Need to put sqlalchemy on the sys.path
 import os
 import sys
-
+import random
+#from config import RDS_URI
+from config import ec_id, ec_pw, ec_host
 home = os.path.split(os.getcwd())[0]
 sqla_dir = os.path.join(home,'sqlalchemy','lib')
 sys.path = [sqla_dir] + sys.path
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.orm import relationship, backref 
-from sqlalchemy.sql import select,join,and_
+from sqlalchemy.orm import sessionmaker, relationship, backref
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.sql import func, and_ #select, join, and_
+from sqlalchemy.exc import IntegrityError, OperationalError
+from sqlalchemy.orm.exc import NoResultFound
 
-import json
-
-__all__ = ['Artist', 'Image', 'session', 'conn', 'select', 'join', 'and_']
+__all__ = ['Artist', 'Image', 'session', 'IntegrityError',  'OperationalError', 'NoResultFound', 'func', 'engine', 'and_'] 
+# might also  need 'Base', 'conn', 'select', 'join', 'and_', 'create_engine', 'sessionmaker', 'engine'
 
 Base = declarative_base()
 
 class Artist(Base):
     __tablename__ = 'artists'
-
     id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True, nullable=False)
-    images = relationship("Image", backref='artists')
+    name = Column(String, unique=True)
+    
+    images = relationship("Image", backref='artist') #, cascade="all, delete", delete-orphan")
 
     def __repr__(self):
-        return "<Artist(name={}>".format(self.name)
+        return "<artist={}>".format(self.name)
 
 class Image(Base):
     __tablename__ = 'images'
     id = Column(Integer, primary_key=True)
-    artist_id = Column(Integer, ForeignKey('artists.id'))
+    artist_id = Column(Integer, ForeignKey('artists.id'), nullable=False)
     link = Column(String)
     width = Column(Integer)
     height = Column(Integer)
-    status = Column(Boolean, default=True)
+    ok = Column(Boolean, default=True)
+    
+    #artist = relationship("Artist", backref=backref('images')) 
 
     def __repr__(self):
-        return "<artist_id={}, width={}, height={}, status={}>".format(self.artist_id, self.width, self.height, self.status)
+        return "<artist_id={}; link={}; ({},{})>".format(self.artist_id, self.link, self.width, self.height)
 
-engine = create_engine('sqlite:///artist.db', echo=True)
-Base.metadata.create_all(engine) # only creates tables if they don't exist
+engine = create_engine('postgresql+psycopg2://{}:{}@{}:5432/artist_images'.format(ec_id, ec_pw, ec_host), echo=False)
+#Base.metadata.create_all(engine) # only creates tables if they don't exist
 
-conn = engine.connect()
+#conn = engine.connect()
 Session = sessionmaker(bind=engine)
 session = Session()
 
-#if run from the command line will populate database from artists.json
+#if run from the command line will print out some information from the database
 if __name__ == '__main__':
 
-    try:
-      with open('artists.json', 'r') as f:
-          artists = json.load(f)
-    except IOError:
-         print "no artists"
-         sys.exit()
 
-    for a in artists:
-        print "artist=",a
-        artist = Artist(name=a)
-        session.add(artist)
-        session.commit()
-        image_list = []
-        for i in artists[a]:
-            image = Image(link=i['link'],width=i['image']['width'],height=i['image']['height'])
-            image_list.append(image)
-        artist.images = image_list
-        session.commit()
+    print("Get some images")
+
+    print("---------------------------------------------------------------")
+    print("---------------------------------------------------------------")
+    rows = session.query(Artist).count()
+    print("Number of artists", rows)
+
+    for n in range(5):
+        r = random.randint(1, rows)
+        artist = session.query(Artist).get(r)
+        if artist:
+            print("artist =",artist.name)
+            for image in artist.images:
+                print("link =",image.link)
+                print("width =",image.width)
+                print("height =",image.height)
+                print("image ok =",image.ok)
+                print("\n")
 
