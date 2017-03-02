4f53e55f99dcc8092fe03e9a81e90db844da5953
slzatz <slzatz@gmail.com>
2017-03-01 21:36:35 -0500

    sonos_track_info.py: change track info to aws mqtt broker

--- sonos_track_info.py
+++ sonos_track_info.py
@@ -187,7 +187,8 @@ while 1:
         # publish to MQTT - could require less code by using micropython mqtt client
         data2 = {'header':'Track Info', 'text':[data['artist'], cur_title], 'pos':2}
         try:
-            mqtt_publish.single(topic, json.dumps(data), hostname=local_mqtt_uri, retain=False, port=1883, keepalive=60)
+            #mqtt_publish.single(topic, json.dumps(data), hostname=local_mqtt_uri, retain=False, port=1883, keepalive=60)
+            mqtt_publish.single(topic, json.dumps(data), hostname=aws_host, retain=False, port=1883, keepalive=60)
             mqtt_publish.single('esp_tft', json.dumps(data2), hostname=aws_host, retain=False, port=1883, keepalive=60)
         except Exception as e:
             print "Exception trying to publish to mqtt broker: ", e
