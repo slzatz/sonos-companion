84e6afa9692deef16f8e1de4744ba2697a0e98cb
slzatz <slzatz@gmail.com>
2017-03-20 08:27:53 -0400

    esp_tft_mqtt.py:no need to order by modified date since shuffling them.  Could probably add days since created to the title of each.

--- esp_tft_mqtt.py
+++ esp_tft_mqtt.py
@@ -163,7 +163,8 @@ def stock_quote():
 
 def todos():
     #pos = 3
-    tasks = session.query(Task).join(Context).filter(and_(Context.title == 'work', Task.priority == 3, Task.completed == None)).order_by(desc(Task.modified))
+    #tasks = session.query(Task).join(Context).filter(and_(Context.title == 'work', Task.priority == 3, Task.completed == None)).order_by(desc(Task.modified))
+    tasks = session.query(Task).join(Context).filter(Context.title == 'work', Task.priority == 3, Task.completed == None)
     titles = ['#'+task.title if task.star else task.title for task in tasks]
     shuffle(titles)
     print(datetime.datetime.now())
@@ -174,7 +175,8 @@ def todos():
 
 def facts():
     #pos = 3
-    tasks = session.query(Task).join(Context).filter(and_(Context.title == 'memory aid', Task.priority == 3, Task.completed == None)).order_by(desc(Task.modified))
+    #tasks = session.query(Task).join(Context).filter(and_(Context.title == 'memory aid', Task.priority == 3, Task.completed == None)).order_by(desc(Task.modified))
+    tasks = session.query(Task).join(Context).filter(Context.title == 'memory aid', Task.priority == 3, Task.completed == None)
     titles = ['#'+task.title if task.star else task.title for task in tasks]
     shuffle(titles)
     print(datetime.datetime.now())
