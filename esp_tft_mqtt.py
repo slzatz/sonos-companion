17c4fbac806032a96ec0d8620f40774bc4b38d5a
slzatz <slzatz@gmail.com>
2017-03-25 19:35:49 -0400

    esp_tft_mqtt.py: '*' added to todos for star and facts use # for bolding

--- esp_tft_mqtt.py
+++ esp_tft_mqtt.py
@@ -173,7 +173,7 @@ def todos():
     #pos = 3
     #tasks = session.query(Task).join(Context).filter(and_(Context.title == 'work', Task.priority == 3, Task.completed == None)).order_by(desc(Task.modified))
     tasks = session.query(Task).join(Context).filter(Context.title=='work', Task.priority==3, Task.completed==None, Task.deleted==False).order_by(desc(Task.star))
-    titles = ['#'+task.title if task.star else task.title for task in tasks]
+    titles = ['*'+task.title if task.star else task.title for task in tasks]
     #shuffle(titles)
     print(datetime.datetime.now())
     print(repr(titles).encode('ascii', 'ignore'))
