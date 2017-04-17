9d24a56c19ecaaae0b092a0b3b723014cc85b2c7
slzatz <slzatz@gmail.com>
2017-04-17 14:11:36 -0400

    esp_tft_mqtt.py: re was missing single digits

--- esp_tft_mqtt.py
+++ esp_tft_mqtt.py
@@ -199,6 +199,8 @@ def facts():
     shuffle(titles)
     titles = titles[:5]
     titles = [re.sub(r'\b\d+\.?\d+M?k?\b', '[  ]',title) for title in titles]
+    titles = [re.sub(r'\b\d+M?k?\b', '[  ]',title) for title in titles] because single digit no decimal place are missed by the first re
+    #re.sub(r'\b\d+\M?k?\b', '[  ]', 'About $650M he33llo 42% 200k I\'m a $32.5 string 30 one 1 two 2')
     #re.sub(r'\b\d+\.?\d+M?k?\b', '[  ]', 'About $650M he33llo 42% 200k I\'m a $32.5 string 30')# captures numbers, numbers with decimal points and numbers followed by 'M' and 'k'
     print(datetime.datetime.now())
     print(repr(titles).encode('ascii', 'ignore'))
