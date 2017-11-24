bd7fc1e490451c0a0fd4f4c133401f186bce0ee1
slzatz <slzatz@gmail.com>
2017-11-24 16:45:00 +0000

    google_calendar_quickstart.py: changed the name of the file looked for in ~/.credentials

--- google_calendar_quickstart.py
+++ google_calendar_quickstart.py
@@ -36,7 +36,7 @@ def get_credentials():
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
     credential_path = os.path.join(credential_dir,
-                                   'calendar-python-quickstart.json')
+                                   'google-calendar.json')
 
     store = Storage(credential_path)
     credentials = store.get()
