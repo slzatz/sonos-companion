7f9892990ba624437db58bb3d5e6ff396162360b
slzatz <slzatz@gmail.com>
2018-05-14 21:39:08 -0400

    sonos_curses.py: updated so that each box is a separate window.  Not extensively tested but seems to work

--- sonos_curses.py
+++ sonos_curses.py
@@ -1,4 +1,25 @@
 #!bin/python
+'''
+0=temp sensors (CT, NYC)
+1=news feeds (WSJ, NYT, ArsTechnica, Reddit All, Twitter)
+2=stock quote
+3=ToDos
+4=sonos status (PLAYING, TRANSITIONING, STOPPED) broadcast by sonos_track_info on topic esp_tft and also on sonos/{loc}/status for esp_tft_mqtt_photos(and lyrics) 
+5=sales forecast
+6=outlook_schedule
+7=artist image
+8=lyrics
+9=track_info broadcast by sonos_track_info.py
+10=sonos status (PLAYING, TRANSITIONING, STOPPED
+11=sales top opportunities
+12=Reminders (alarms) 
+13=Ticklers
+14=Facts
+15=weather/tides
+16=Industry
+17=temp sensor
+'''
+
 import curses
 from config import aws_mqtt_uri 
 import paho.mqtt.client as mqtt
@@ -8,12 +29,13 @@ from datetime import datetime
 import time
 import re
 
-MAX_HEIGHT = 20 
-MAX_WIDTH = 50
-MIN_WIDTH = 30
-
-info_sources = [1,2,3,5,6,11,15]
-location = {1:3, 2:12, 3:18, 5:30, 6:38, 11:50, 15:62}
+layout   = { 1:{'y':2,  'h':9},
+             2:{'y':12, 'h':5},
+             3:{'y':18, 'h':10},
+             5:{'y':29, 'h':5},
+             6:{'y':35, 'h':11},
+            11:{'y':47, 'h':16},
+            15:{'y':64, 'h':6}}
 
 # paho stuff
 info_topic = "esp_tft" # should be changed to "info"
@@ -27,6 +49,11 @@ def on_disconnect():
     print("Disconnected from mqtt broker")
 
 screen = curses.initscr()
+curses.curs_set(0)
+boxes = {}
+#curses.newwin(nlines, ncols, begin_y, begin_x)
+for b in layout:
+    boxes[b] = curses.newwin(layout[b]['h'], 70, layout[b]['y'], 1)
 
 def main(screen):
     screen.clear()
@@ -73,36 +100,36 @@ def on_message(client, userdata, msg):
 
     #if topic in (info_topic, image_topic):
     k = z.get('pos')
-    if not k in info_sources: ####################
+    if not k in layout: ####################
         return
 
     dest = z.get('dest')
 
     if topic==info_topic:
 
+        box = boxes[k]
+        box.clear()
+        box.box()
+
         header = "{} [{}] {}".format(z.get('header', 'no source'), k, dest if dest else " ") # problem if dest None would like to know what it was
         #screen.clear()
-        screen.addstr(0, 0, header, curses.A_BOLD) #(y,x)
+        screen.move(0,0)
+        screen.clrtoeol()
+        screen.addstr(0,0, header, curses.A_BOLD) #(y,x)
         screen.refresh()
-        #screen.addstr(20,20, "Hello World", curses.A_BOLD)
-        #screen.clear()
-        #screen.refresh() #not necessary I don't think
-        line_height = 1
-        line_widths = [0] # for situation when text = [''] otherwise line_widths = [] and can't do max
         bullets = z.get('bullets', True)
 
-        #n = line_height #20
-        n = location.get(k, 1)
+        n = 1
         for item in z.get('text',['No text']): 
             if not item.strip():
-                n+=line_height
+                n+=1
                 continue
             #font.set_bold(False)
-            max_chars_line = 66        
+            max_chars_line = 50        
             indent = 1
             #n+=4 if bullets else 0 # makes multi-line bullets more separated from prev and next bullet
 
-            if n+line_height > location.get(k,1) + MAX_HEIGHT:
+            if n+1 == layout[k]['h']:
                 break
 
             if item[0] == '#':
@@ -121,11 +148,12 @@ def on_message(client, userdata, msg):
             #    max_chars_line+= 1 
             #    indent = 10
 
-            lines = textwrap.wrap(item, max_chars_line) # if line is just whitespace it returns []
+            # if line is just whitespace it returns []
+            lines = textwrap.wrap(item, max_chars_line)
 
             for l,line in enumerate(lines):
 
-                if n+line_height > location.get(k,1) + MAX_HEIGHT: #20
+                if n+1 == layout[k]['h']:
                     break
 
                 if l:
@@ -136,48 +164,30 @@ def on_message(client, userdata, msg):
                 xx = 0
                 for phrase in phrases:
                     try:
-                        #screen.addstr(n, indent + xx, phrase[1], curses.A_BOLD) #(y,x)
-                        screen.addstr(n, indent + xx, phrase[1]) #(y,x)
+                        box.addstr(n, 1 + xx, phrase[1]) #(y,x)
                     except Exception as e:
-                         #screen.addstr(80, 0, str(e))
-                         #screen.addstr(80, 0, "ERROR", curses.A_REVERSE)
                          pass
-                    screen.refresh()
+                    box.refresh()
                     xx+= len(phrase[1])
 
-                line_widths.append(xx)
-                n+=line_height
+                n+=1
 
-        # determine the size of the rectangle for foo and its line border
-        max_line = max(line_widths)
-        if max_line > MAX_WIDTH:
-            max_line = MAX_WIDTH
-        elif max_line < MIN_WIDTH:
-            max_line = MIN_WIDTH
-
-        # item is the last item and if the last item is white space n gets incremented unnecessarily and this 'un'increments it
+        # item is the last item and if the last item is white space n gets
+        # incremented unnecessarily and this 'un'increments it
         if not item.strip():
-            n-=line_height
-        #height = min(n+12, MAX_HEIGHT)
-        #screen.border(0)
-        box1 = curses.newwin(n-location.get(k,1)+2, max_line+20, location.get(k, 1)-1, 0)
-        box1.box()
-        box1.overlay(screen)
-        screen.refresh()
-        #box1.refresh()
+            n-=1
 
         # put time in upper right of box
         t = datetime.now().strftime("%I:%M %p")
         t = t[1:] if t[0] == '0' else t
         t = t[:-2] + t[-2:].lower()
-        #screen.addstr(20,20, "Hello World", curses.A_BOLD)
-        #screen.refresh()
 
 print("about to connect to mqtt broker")
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect(aws_mqtt_uri, 1883, 60)
+# I think I need a client loop here to connect
 time.sleep(10)
 screen.clear()
 screen.addstr(0,0, "Hello Steve", curses.A_BOLD)
