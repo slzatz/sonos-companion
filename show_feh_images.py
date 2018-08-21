526fd1c44fb40ca49ef44d54c835386e91115824
slzatz <slzatz@gmail.com>
2018-08-21 16:40:35 -0400

    show_feh_images and show_images_test.py: both cleaned up and minimally tested

--- show_feh_images.py
+++ show_feh_images.py
@@ -1,70 +1,142 @@
 #!bin/python
 '''
-python3 script: displays infoboxes with key determing which box is displayed
-See below for mapping from pos (int) to topic of infobox
+python3 script: places images in sonos-companion/images where feh
+in slide show mode can display the single image automatically
+feh is launched with feh -R X which causes it to reload every X
+seconds.  
+
+This script listens for the trackinfo broadcast by local raspberry
+pi to AWS mqtt broker under the topic sonos/{loc}/track
 '''
 
-from config import aws_mqtt_uri 
 import paho.mqtt.client as mqtt
 import json
 import time
 import wand.image
 import requests
 from io import BytesIO
+from apiclient import discovery #google custom search api
+import httplib2 #needed by the google custom search engine module apiclient
+from config import aws_mqtt_uri, google_api_key
+from artist_images_db import *
 
-image_topic = "images"
+trackinfo = {"artist":None, "track_title":None, "lyrics":None, "images":[]}
+new_track_info = False
 
-def display_image(x):
+with open('location') as f:
+    location = f.read().strip()
 
-    print(x)
+sonos_track_topic = "sonos/{}/track".format(location)
+
+def display_image(image):
+    '''image = sqlalchemy image object'''
+    print(image.link)
     try:
-        response = requests.get(x, timeout=5.0)
-    except Exception as e:
-        print("response = requests.get(url) generated exception: ", e)
-        # in some future better world may indicate that the image was bad
-
+        response = requests.get(image.link, timeout=5.0)
+    except (requests.exceptions.ConnectionError,
+            requests.exceptions.TooManyRedirects,
+            requests.exceptions.ChunkedEncodingError,
+            requests.exceptions.ReadTimeout) as e:
+        print(f"requests.get({image.link}) generated exception:\n{e}")
+        image.ok = False
+        session.commit()
+        print(f"{image.link} ok set to False")
         return
-    else:     
-        try:
-            img = wand.image.Image(file=BytesIO(response.content))
-        except Exception as e:
-            print("img = wand.image.Image(file=BytesIO(response.content)) generated exception from url:", x, "Exception:", e)
-            # in some future better world may indicate that the image was bad
 
-            return
+    print(f"status code = {response.status_code}")
+    if response.status_code != 200:
+        print(f"{image.link} returned a {response.status_code}")
+        image.ok = False
+        session.commit()
+        print(f"{image.link} ok set to False")
+        return
+        
+    if response.content.isascii():
+        print(f"{image.link} returned ascii text and not an image")
+        image.ok = False
+        session.commit()
+        print(f"{image.link} ok set to False")
+        return
+
+    # this try/except is needed for occasional bad/unknown file format
+    try:
+        img = wand.image.Image(file=BytesIO(response.content))
+    except Exception as e:
+        print(f"wand.image.Image(file=BytesIO(response.content))"\
+              f"generated exception from {image.link} {e}")
+        image.ok = False
+        session.commit()
+        print(f"{image.link} ok set to False")
+        return
+
+    ww = img.width
+    hh = img.height
+    sq = ww if ww <= hh else hh
+    t = ((ww-sq)//2,(hh-sq)//2,(ww+sq)//2,(hh+sq)//2) 
+    img.crop(*t)
+    # resize should take the image and enlarge it without cropping 
+    img.resize(800,800) #400x400
+    img.save(filename = "images/zzzz")
+    img.close()
+
+def get_artist_images(name):
+
+    print(f"**************Google Custom Search Engine Request for {name} **************")
+    http = httplib2.Http()
+    service = discovery.build('customsearch', 'v1',
+                              developerKey=google_api_key, http=http)
+    z = service.cse().list(q=name, searchType='image', imgType='face',
+                           imgSize='xlarge', num=10,
+                           cx='007924195092800608279:0o2y8a3v-kw').execute() 
 
     try:
-        ww = img.width
-        hh = img.height
-        sq = ww if ww <= hh else hh
-        t = ((ww-sq)//2,(hh-sq)//2,(ww+sq)//2,(hh+sq)//2) 
-        img.crop(*t)
-        # resize should take the image and enlarge it without cropping so will fill vertical but leave space for lyrics
-        img.resize(800,800) #400x400
-        #conv_img = img.convert('bmp')
-        img.save(filename = "images/zzzz")
-        img.close()
+        a = session.query(Artist).filter(func.lower(Artist.name)==name.lower()).one()
+    except NoResultFound:
+        print("Don't have that name in db so created it")
+        a = Artist()
+        a.name = name
+        session.add(a)
+        session.commit()
     except Exception as e:
-        print("img.transfrom or img.convert error:", e)
-        # in some future better world may indicate that the image was bad
+        print("a = session.query(Artist).filter(func.lower.. error:", e) 
+        return []
 
-        return
+    # must delete images before you can add new whole new set of images
+    session.query(Image).filter_by(artist_id=a.id).delete()
+    session.commit()
+
+    images = []
+
+    for data in z.get('items', []): #['items']: # only empty search should be on artist='' and I think I am catching that but this makes sure
+        image=Image()
+        image.link = data['link']
+        image.width = data['image']['width']
+        image.height = data['image']['height']
+        image.ok = True
+        images.append(image)
+
+    a.images = images
+    session.commit()
+            
+    print("images = ", images)
+    return images 
 
 def on_connect(client, userdata, flags, rc):
     print("(Re)Connected with result code "+str(rc)) 
 
     # Subscribing in on_connect() means that if we lose the 
     # connection and reconnect then subscriptions will be renewed.
-    #client.subscribe([(info_topic, 0)])
-    client.subscribe([(image_topic, 0)])
+    client.subscribe([(sonos_track_topic, 0)])
+    #client.subscribe([(image_topic, 0)])
 
 def on_disconnect():
     print("Disconnected from mqtt broker")
 
 def on_message(client, userdata, msg):
+    global new_track_info
     topic = msg.topic
     body = msg.payload
-    #print(topic+": "+str(body))
+    print(topic+": "+str(body))
 
     try:
         z = json.loads(body)
@@ -72,8 +144,35 @@ def on_message(client, userdata, msg):
         print("error reading the mqtt message body: ", e)
         return
 
-    uri = z.get('uri')
-    display_image(uri)
+    print("z = json.loads(body) =",z)
+
+    artist = z.get("artist", "")
+    track_title = z.get("title", "")
+
+    print("artist =",artist)
+    print("track_title =",track_title)
+
+    try:
+        a = session.query(Artist).filter(func.lower(Artist.name)==artist.lower()).one()
+    except NoResultFound:
+        # must be new artist so get images
+        images = get_artist_images(artist)
+        #images = []
+        if not images:
+            print("Could not find images for {}".format(artist))
+    except Exception as e:
+        print("error trying to find artist:", e)
+        images = []
+    else:
+        images = [im for im in a.images if im.ok]
+        if len(images) < 5:
+            print("fewer than 5 images so getting new set of images for artist")
+            images = get_artist_images(artist)
+            if not images:
+                print "Could not find images for {}".format(artist)
+
+    new_track_info = True
+    trackinfo.update({"artist":artist, "track_title":track_title, "images":images})
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -85,6 +184,16 @@ while time.time() < t0 + 10:
     client.loop(timeout = 1.0)
     time.sleep(1)
 
+images = []
 while 1:
     client.loop(timeout = 0.25) #was 1.0
+    if new_track_info:
+        images = trackinfo["images"][::]
+        new_track_info = False
+    if images:
+        if time.time() > t0 + 10:
+            display_image(images.pop())
+            t0 = time.time()
+    else:
+        images = trackinfo["images"][::]
     time.sleep(.1)
--- show_images_test.py
+++ show_images_test.py
@@ -11,83 +11,120 @@ import time
 import wand.image
 import requests
 from io import BytesIO
+from apiclient import discovery #google custom search api
+import httplib2 #needed by the google custom search engine module apiclient
+from config import google_api_key
 from artist_images_db import *
+from artists import artists
 import random
 
-def display_image(x):
+num_artists = session.query(Artist).count()
 
-    img_link = x.link
-    print(img_link)
+def display_image(image):
+    '''image = sqlalchemy image object'''
+    print(image.link)
     try:
-        response = requests.get(img_link, timeout=5.0)
-    except Exception as e:
-        print("response = requests.get(url) generated exception: ", e)
-        x.ok = False
+        response = requests.get(image.link, timeout=5.0)
+    #except Exception as e:
+    except (requests.exceptions.ConnectionError,
+            requests.exceptions.TooManyRedirects,
+            requests.exceptions.ReadTimeout) as e:
+
+        print(f"requests.get({image.link}) generated exception:\n{e}")
+        image.ok = False
         session.commit()
-        print(f"{img_link} ok set to False")
+        print(f"{image.link} ok set to False")
         return
 
     print(f"status code = {response.status_code}")
-    #if response.status_code in (403,404, 503):
     if response.status_code != 200:
-        print(f"{img_link} returned a {response.status_code}")
-        x.ok = False
+        print(f"{image.link} returned a {response.status_code}")
+        image.ok = False
         session.commit()
-        print(f"{img_link} ok set to False")
+        print(f"{image.link} ok set to False")
         return
         
+    if response.content.isascii():
+        print(f"{image.link} returned ascii text and not an image")
+        image.ok = False
+        session.commit()
+        print(f"{image.link} ok set to False")
+        return
+
+    # this try/except is needed for occasional bad/unknown file format
     try:
         img = wand.image.Image(file=BytesIO(response.content))
     except Exception as e:
-        print("img = wand.image.Image(file=BytesIO(response.content))"\
-              "generated exception from url:", x, "Exception:", e)
-        # in some future better world may indicate that the image was bad
-        print(f"{img_link} is bad")
-        x.ok = False
+        print(f"wand.image.Image(file=BytesIO(response.content))"\
+              f"generated exception from {image.link} {e}")
+        image.ok = False
         session.commit()
-        print(f"{img_link} ok set to False")
+        print(f"{image.link} ok set to False")
         return
 
-    try:
-        ww = img.width
-        hh = img.height
-        sq = ww if ww <= hh else hh
-        t = ((ww-sq)//2,(hh-sq)//2,(ww+sq)//2,(hh+sq)//2) 
-        img.crop(*t)
-        # resize should take the image and enlarge it without cropping 
-        img.resize(800,800) #400x400
-    except Exception as e:
-        print("image crop or resize error ", e)
-        x.ok = False
-        session.commit()
-        print(f"{img_link} ok set to False")
-        return
+    ww = img.width
+    hh = img.height
+    sq = ww if ww <= hh else hh
+    t = ((ww-sq)//2,(hh-sq)//2,(ww+sq)//2,(hh+sq)//2) 
+    img.crop(*t)
+    # resize should take the image and enlarge it without cropping 
+    img.resize(800,800) #400x400
+    img.save(filename = "images/zzzz")
+    img.close()
 
-    try:
-        img.save(filename = "images/zzzz")
-        img.close()
-    except Exception as e:
-        print("img.save or img.close error:", e)
-        print(f"{img_link} is bad")
-        x.ok = False
-        session.commit()
-        print(f"{img_link} ok set to False")
-        #return
+def get_artist_images(artist):
 
-ok_images = []
+    print(f"**************Google Custom Search Engine Request for {artist.name} **************")
+    http = httplib2.Http()
+    service = discovery.build('customsearch', 'v1',  developerKey=google_api_key, http=http)
+    z = service.cse().list(q=artist.name, searchType='image', imgType='face',
+                           imgSize='xlarge', num=10, 
+                           cx='007924195092800608279:0o2y8a3v-kw').execute() 
+
+
+    # must delete images before you can add new whole new set of images
+    session.query(Image).filter_by(artist_id=artist.id).delete()
+    session.commit()
+
+    images = []
+
+    #['items']: # only empty search should be on artist='' 
+    # and I think I am catching that but this makes sure
+    for data in z.get('items', []): 
+        image=Image()
+        image.link = data['link']
+        image.width = data['image']['width']
+        image.height = data['image']['height']
+        image.ok = True
+        images.append(image)
+
+    artist.images = images
+    session.commit()
+            
+    #print("images = ", images)
+    #return images 
+    artists.append(artist.name)
+
+images = []
 while 1:
-    if ok_images:
-        image = ok_images.pop()
-        display_image(image)
+    if images:
+        display_image(images.pop())
     else:
-        rand = random.randrange(0, session.query(Artist).count()) 
-        artist = session.query(Artist).get(rand)
+        if artists:
+            name = artists.pop()
+            artist = session.query(Artist).filter(func.lower(Artist.name)==name.lower()).one()
+        else:
+            rand = random.randrange(0, num_artists) 
+            artist = session.query(Artist).get(rand)
+            if not artist:
+                continue
+
         print(artist.name)
-        images = artist.images
-        ok_images = [im for im in images if im.ok] 
-        if len(ok_images) < 8:
+        images = [im for im in artist.images if im.ok] 
+        if len(images) < 8:
             # find new uris
             print(f"{artist.name} has less than 8 images")
             # need to delete the bad ones and get new ones
+            get_artist_images(artist)
         
-    time.sleep(5)
+    time.sleep(3)
