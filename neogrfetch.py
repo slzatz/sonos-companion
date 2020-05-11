a53a945afb3a05304f41f17e74a87cd5dce0f4db
slzatz <slzatz@gmail.com>
2020-05-11 19:59:33 -0400

    neogrfetch.py: can now handle name on command line and still randomizes quotes when no argument.  Switched sql to replace multiple calls to inner joins

--- neogrfetch.py
+++ neogrfetch.py
@@ -14,58 +14,69 @@ cur = conn.cursor()
 
 if __name__ == "__main__":
     '''  
-    id integer PRIMARY KEY,
-    book_gr_id INTEGER NOT NULL,
-    quote text NOT NULL,
-    FOREIGN KEY(book_gr_id) REFERENCES books (gr_id),
-    UNIQUE(quote)
+    Displays image, quote and bio of select authors
     '''
 
     line_count = 2
     x = get_screen_size()
-    author_image_size = 400
 
+    if len(sys.argv) == 1:
+
+        cur.execute("SELECT book_gr_id,quote FROM quotes ORDER BY RANDOM() LIMIT 1;")
+        row = cur.fetchone()
+        book_gr_id = row[0]
+        quote = row[1]
+
+        sql = "SELECT authors.id,authors.name,authors.bio, books.title FROM books " \
+              "INNER JOIN authors ON authors.id=books.author_id AND books.gr_id=?;" #(book_gr_id)
+
+        cur.execute(sql, (book_gr_id,))
+        row = cur.fetchone()
+        author_id, name, bio, title = row 
+
+    else:
+
+        sql = "SELECT authors.id,authors.name,authors.bio,books.title, books.gr_id FROM books " \
+              "INNER JOIN authors ON authors.id=books.author_id AND authors.name=? ORDER BY RANDOM() LIMIT 1;"
+
+        cur.execute(sql, (sys.argv[1].title(),))
+        row = cur.fetchone()
+        author_id, name, bio, title, gr_id = row 
+
+        cur.execute("SELECT quote FROM quotes WHERE book_gr_id=?ORDER BY RANDOM() LIMIT 1;", (gr_id,))
+        row = cur.fetchone()
+        quote = row[0]
+
+    cur.execute("SELECT image,size FROM images WHERE author_id=?", (author_id,))
+    row = cur.fetchone()
+    image = BytesIO(row[0])
+    author_image_size = row[1]
     indent_cols = ceil(author_image_size/x.cell_width)
     indent = indent_cols * ' '
     max_chars_line = x.cols - 5
-    authoriz_image_size = 400
 
-    cur.execute("SELECT book_gr_id,quote FROM quotes ORDER BY RANDOM() LIMIT 1;")
-    row = cur.fetchone()
-
-    book_gr_id = row[0]
-    quote = textwrap.wrap(row[1], max_chars_line, initial_indent=indent, subsequent_indent=indent)
+    quote = textwrap.wrap(quote, max_chars_line, initial_indent=indent, subsequent_indent=indent)
     line_count += len(quote)
     quote = "\n".join(quote)
 
-    cur.execute("SELECT title,author_id FROM books WHERE gr_id=?", (book_gr_id,))
-    row = cur.fetchone()
-    title = row[0]
-    author_id = row[1]
-
-    cur.execute("SELECT name,bio FROM authors WHERE id=?", (author_id,))
-    row = cur.fetchone()
-    name = row[0]
-    bio = textwrap.wrap(row[1], max_chars_line, initial_indent=indent, subsequent_indent=indent)
+    # just using one sentence bios but bios have up to 10 sentences
+    bio = bio[:bio.find('.') + 1]
+    bio = textwrap.wrap(bio, max_chars_line, initial_indent=indent, subsequent_indent=indent)
     line_count += len(bio)
     bio = "\n".join(bio)
 
-    cur.execute("SELECT image FROM images WHERE author_id=?", (author_id,))
-    row = cur.fetchone()
-    image = BytesIO(row[0])
     print() # line feed
-
     q = f"\x1b[3m{quote}\x1b[0m\n{indent}-- \x1b[1m{title} by {name}\x1b[0m\n\n{bio}"
     print(q)
 
-    sys.stdout.buffer.write(f"\x1b[{line_count}A".encode('ascii')) # move back up 9 lines
+    sys.stdout.buffer.write(f"\x1b[{line_count}A".encode('ascii')) # move back up line_count lines
     sys.stdout.buffer.write(b"\x1b[7B") #move down 8 lines to near middle of image
     print("  retrieving photo ...")
     sys.stdout.buffer.write(b"\x1b[8A") # move back up 9 lines
 
     show_image(image)
-
     print()
 
-    if line_count > 22: 
-        print((line_count-22)*"\n")
+    image_rows = author_image_size//x.cell_height
+    if line_count > image_rows:
+        print((line_count-image_rows)*"\n")
