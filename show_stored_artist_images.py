a20cc14a972beb2b77623d353ed7ba95c7b159fb
slzatz <slzatz@gmail.com>
2020-07-18 16:45:46 -0400

    show_stored_artist_images.py: done a while ago - need to review but displays images stored in database if available or from Web

--- show_stored_artist_images.py
+++ show_stored_artist_images.py
@@ -2,6 +2,7 @@
 
 '''
 Uses kitty graphics api to display either jpegs or png images from web
+or display pngs stored in the database
 search for artist whose music is playing on Sonos.
 '''
 import time
@@ -12,7 +13,9 @@ from ipaddress import ip_address
 import psycopg2
 from config import speaker, sonos_image_size, ec_id, ec_pw, ec_host #speaker = "192.168.86.23" -> Office2
 from display_image import display_image, display_blended_image, generate_image, show_image, blend_images
-home = os.path.split(os.getcwd())[0]
+from pathlib import Path
+home = str(Path.home())
+#home = os.path.split(os.getcwd())[0]
 sys.path = [os.path.join(home, 'SoCo')] + sys.path
 import soco
 #from soco import config as soco_config
@@ -29,6 +32,9 @@ cur = conn.cursor()
 
 if __name__ == "__main__":
 
+    num_transport_errors = 0
+    num_track_errors = 0
+
     try:
         ip_address(speaker)
     except ValueError:
@@ -43,16 +49,18 @@ if __name__ == "__main__":
     alpha = 1.1
     s = ""
 
-    #last_image = None # for blended images - not doing it now
-
     while 1:
         try:
             state = master.get_current_transport_info()['current_transport_state']
         except Exception as e:
             print(f"Encountered error in state = master.get_current_transport_info(): {e}")
             state = 'ERROR'
-            time.sleep(1)
-            continue
+            num_transport_errors += 1
+            if num_transport_errors < 3:
+                time.sleep(1)
+                continue
+            else:
+                sys.exit(1)
 
         if state == 'PLAYING':
 
@@ -60,8 +68,12 @@ if __name__ == "__main__":
                 track = master.get_current_track_info()
             except Exception as e:
                 print("Encountered error in track = master.get_current_track_info(): {e}")
-                time.sleep(1)
-                continue
+                num_track_errors += 1
+                if num_track_errors < 3:
+                    time.sleep(1)
+                    continue
+                else:
+                    sys.exit(1)
 
             title = track.get('title', '')
             
@@ -75,21 +87,19 @@ if __name__ == "__main__":
                     time.sleep(5)
                     continue
 
-                #sql = "SELECT id,name FROM artists WHERE LOWER(name)=LOWER(%s);"
                 sql = "SELECT id FROM artists WHERE LOWER(name)=LOWER(%s);"
                 cur.execute(sql, (artist,))
                 row = cur.fetchone()
                 if row:
-                    #artist_id, name = row
                     artist_id = row[0]
                 else:
                     print(f"Can't find {artist}!!")
+                    continue
 
                 cur.execute(f"SELECT id,link FROM images WHERE artist_id={artist_id}")
                 all_rows = cur.fetchall()
                 rows = all_rows[::]
                 alpha = 1.1 
-                #print(f"\x1b[1m{title} {artist}\x1b[0m\n")
 
             if rows:
                 if alpha > 1.0:
@@ -102,13 +112,10 @@ if __name__ == "__main__":
                         r = cur.fetchone()
                         if r:
                             s = "This image is being stored as BYTES in the database"
-                            #print("This image is being stored as BYTES in the database")
-                            #show_image(BytesIO(r[0]))
                             img_current = BytesIO(r[0])
                             print()
                         else:
                             s = "This image is being stored as just a URL in the database"
-                            #print("This image is being stored as just a URL in the database")
                             img_current = generate_image(row[1], sonos_image_size, sonos_image_size)
                             print()
                         if img_current:
@@ -124,22 +131,16 @@ if __name__ == "__main__":
                         sys.stdout.buffer.write(b"\x1b[H")
                         show_image(img_blend)
                         print(f"\n\x1b[1m{title} {artist}\x1b[0m\n{s}")
-                        alpha += .015 # .025 goes will with time.sleep(.05)
+                        alpha += .015 
                 # I believe this is the path when the title changes
                 elif img_current:
-                    #alpha = 1.1
-                    #display_image(image.link, image_size, image_size)
                     sys.stdout.buffer.write(b"\x1b[H")
                     show_image(img_current)
                     print(f"\n\x1b[1m{title} {artist}\x1b[0m\n{s}")
                     alpha += 0.25
-
-                #print(f"\n{artist}: {title}\n{image.link}")
-
                 
             else:
                 rows = all_rows[::]
-
 
             time.sleep(.01) 
 
